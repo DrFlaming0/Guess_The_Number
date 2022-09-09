a0950245bcde228626b07afdc4cd064ca1863c73
Alexander Nielsen <alexander.nielsen3@learnet.se>
2022-09-09 15:36:13 +0200

    Första delen

---
+++ Number.js
@@ -0,0 +1,10 @@
+let difficulty  = ["Easy", "Medium", "Hard"];
+
+let prompt = require("prompt-sync") ();
+prompt("Choose the difficulty by typing either easy, medium or hard");
+
+
+
+
+Hello
+> hhhuihuih
