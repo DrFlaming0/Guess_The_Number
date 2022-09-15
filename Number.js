16712872b8ad1844128b06361a747f8315c752f0
Alexander Nielsen <alexander.nielsen3@learnet.se>
2022-09-15 10:34:19 +0200

    Slut på projektet

--- Number.js
+++ Number.js
@@ -1,5 +1,5 @@
 const prompt = require("prompt-sync") ();
-let player = { Health: 5, winStreaks: 0 };
+let player = { health: 5, winStreaks: 0 };
 const congrats = 
 "\n_______  _____  __   _  ______  ______ _______ _______ _     _        _______ _______ _____  _____  __   _ _______ | "+ 
 "\n|       |     | | \\  | |  ____ |_____/ |_____|    |    |     | |      |_____|    |      |   |     | | \\  | |______ | " +
@@ -20,7 +20,7 @@ function gameMode(numberRange, instructions) {
     let randomGeneratedNumber = Math.round(Math.random() * numberRange);
     console.log(instructions);
            
-    while (player.Health !== 0 && playerGuess !== randomGeneratedNumber) {
+    while (player.health !== 0 && playerGuess !== randomGeneratedNumber) {
         var playerGuess = parseInt(prompt("> "));
    
         if (playerGuess === randomGeneratedNumber) {
@@ -28,14 +28,14 @@ function gameMode(numberRange, instructions) {
             console.log(congrats, "\nYou currently have", player.winStreaks, "winstreaks!");
         }else if (playerGuess > randomGeneratedNumber) {
             console.log("Less than", playerGuess + "\n");
-            player.Health--;
-            console.log(player.Health, "hearts remaining");
+            player.health--;
+            console.log(player.health, "hearts remaining");
         }else if (playerGuess < randomGeneratedNumber) {
             console.log("Greater than", playerGuess + "\n");
-            player.Health--;
-            console.log(player.Health, "hearts remaining");
+            player.health--;
+            console.log(player.health, "hearts remaining");
         };
-        if (player.Health === 0) {console.log(gameOver, "\n\nThe random number was", randomGeneratedNumber)};
+        if (player.health === 0) {console.log(gameOver, "\n\nThe random number was", randomGeneratedNumber)};
     };
 };
 
@@ -43,7 +43,7 @@ const startGame = prompt("Press the enter key on your keyboard to play the game!
 
 do {
     if (startGame === "") {
-        player.Health = 5;
+        player.health = 5;
         console.log("\nChoose the game difficulty (Easy, Medium, Hard)");
         const gameDifficulty = prompt("> ");
 
@@ -58,7 +58,7 @@ do {
         if (continueGame === "Yes" || continueGame === "yes") { continueGame = true; }
         else { continueGame = false; console.log("");};
     }
-} while (continueGame === true);
+} while (continueGame);
 
 
 
--- test.js
+++ test.js
@@ -1,24 +1,6 @@
-//if & else statements\\
-const idag = "soligt";
-
-if (idag === "regnar") {
-    console.log("Ta med ett paraply");
-} else {
-    console.log("Äsch, jag behöver inget paraply");
+function addShoeSizeWithAge(shoeSize, age) {
+    console.log(shoeSize + age)
 }
 
-
-// const imorgon = "soligt";
-
-// if (imorgon === "soligt") {
-//     console.log("Ta på dig shorts")
-// }
-
-//while loops\\
-
-
-//Do while loops\\
-
-
-//Arrays\\
-
+addShoeSizeWithAge(40, 16) //Alexander
+addShoeSizeWithAge(50, 17) //Ramin
